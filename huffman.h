b7045483b8951ba9e8ecd3d81b513f7e2e357408
clcondorcet <clement.rostagni@free.fr>
2020-11-20 18:10:56 +0100

    D and E of the project (with struct of C)

--- huffman.h
+++ huffman.h
@@ -5,4 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+Tree create_huffman_tree(List * occurrencesList);
+void printDico(Node * tree, FILE * file, char * chars);
+int createDico(Tree huffmanTree);
+
 #endif /* HUFFMAN_H */
--- structures.h
+++ structures.h
@@ -1,6 +1,27 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
+typedef struct Element{
+    char chara;
+    int occurrences;
+    struct Element * next;
+}Element;
 
+typedef Element* List;
+
+typedef struct Node{
+    char chara;
+    int occurrences;
+    int haveChara;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+typedef Node* Tree;
+
+Element * create_element(char chara, int occurrences);
+Element * popMin(List * list);
+Node * create_node(char chara, int haveChara, int occurrences, Node * left, Node * right);
+int treeDeapth(Node * node);
 
 #endif /* STRUCTURES_H */
