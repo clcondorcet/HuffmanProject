47e3d8d81a992a5b3bef0826b60d7d20e6f0565c
steroidi884 <64490661+steroidi884@users.noreply.github.com>
2020-11-20 19:13:34 +0100

    Update huffman.c

--- huffman.c
+++ huffman.c
@@ -1,1 +1,30 @@
 #include "huffman.h"
+
+void translate_texte_with_huffman(){
+
+    FILE* texte = fopen("texte.txt", "r");
+    FILE* code = fopen("dico.txt", "r");
+    FILE* encode_texte = fopen("compressed.txt", "w+");
+
+    char letter = fgetc(texte);
+    while (letter != EOF){
+
+        char* take_info = (char*)malloc(12*sizeof(char));
+        do{
+            fgets(take_info, 12, code);
+        }while(letter != take_info[0]);
+        rewind(code);
+
+        int i = 1;
+        while(take_info[i] == '0' || take_info[i] == '1'){
+            fprintf(encode_texte, "%c", take_info[i]);
+            i++;
+        }
+        free(take_info);
+        letter = fgetc(texte);
+    }
+
+    fclose(code);
+    fclose(texte);
+    fclose(encode_texte);
+}
