9010bf3f9c10de1e0e17b64f80050ab545135a80
steroidi884 <64490661+steroidi884@users.noreply.github.com>
2020-11-27 19:18:23 +0100

    Update huffman.h

--- huffman.h
+++ huffman.h
@@ -7,7 +7,8 @@
 
 /**
  * @brief Function that translates a text into a binary sequence based on a Huffman dictionary
- * 
+ * The function has no parameters because we open and close the file in the function
+ * The function take a letter of the text, find her binary sequence and print the binary sequence in a file
  */
 void translate_texte_with_huffman();
 
