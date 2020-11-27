c631b7cd007a60f1e3ee3a7af3c4a4f2c656f13f
clcondorcet <clement.rostagni@free.fr>
2020-11-27 17:13:05 +0100

    adding docs

--- huffman.h
+++ huffman.h
@@ -5,8 +5,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * @brief Create a huffman tree object.
+ * 
+ * @param occurrencesList 
+ * @return Tree 
+ */
 Tree create_huffman_tree(List * occurrencesList);
+
+/**
+ * @brief A recursive function used by createDico to print the huffman tree in a file.
+ * 
+ * @param tree 
+ * @param file 
+ * @param chars 
+ */
 void printDico(Node * tree, FILE * file, char * chars);
+
+/**
+ * @brief Print a huffman tree in dico.txt.
+ * 
+ * @param huffmanTree 
+ * @return 0 if error, 1 if all is good
+ */
 int createDico(Tree huffmanTree);
 
 #endif /* HUFFMAN_H */
