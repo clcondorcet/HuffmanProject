b0585af9e295dd30ab8ccdb5df1e529e64cebc97
steroidi884 <64490661+steroidi884@users.noreply.github.com>
2020-12-11 03:09:12 +0100

    Update structures.c

--- structures.c
+++ structures.c
@@ -175,3 +175,19 @@ void free_queueChar(QueueChar * queue){
     }
     free(queue);
 }
+
+Node_newType* create_Node_newType(char letter, Element_newType* list, Node_newType* theRight, Node_newType* theLeft){
+    Node_newType* newNode = (Node_newType*)malloc(sizeof(Node_newType));
+    newNode->data = letter;
+    newNode->l = list;
+    newNode->right = theRight;
+    newNode->left = theLeft;
+    return newNode;
+}
+
+Element_newType* create_Element_newType(int nbr, Element_newType* list){
+    Element_newType* newElement = (Element_newType*)malloc(sizeof(Element_newType));
+    newElement->boolean = nbr;
+    newElement->next = list;
+    return newElement;
+}
